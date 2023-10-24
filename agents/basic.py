091385a488dc504e6726455072f92ac4536863b7
Bowen Li <bowenli2@andrew.cmu.edu>
2023-10-24 14:27:40 -0400

    polished agent action code

--- agents/basic.py
+++ agents/basic.py
@@ -1,4 +1,6 @@
 import torch
+from torch.distributions import Categorical
+from core.config import *
 
 class Agent:
     def __init__(self, type, size, id, world_state_matrix, debug=False):
@@ -43,3 +45,35 @@ class Agent:
 
     def get_movable_area(self, world_state_matrix):
         pass
+
+    def get_action(self, local_action_dist):
+        if not torch.any(local_action_dist):
+            return self.get_global_action()
+        else:
+            # sample from the local planner
+            normalized_action_dist = local_action_dist / local_action_dist.sum()
+            dist = Categorical(normalized_action_dist)
+            # Sample an action index from the distribution
+            action_index = dist.sample()
+            # Get the actual action from the action space using the sampled index
+            return self.action_space[action_index]
+
+    def move(self, action, ped_layer):
+        curr_pos = torch.nonzero((ped_layer==TYPE_MAP[self.type]).float())[0]
+        assert torch.all(self.pos == curr_pos)
+        next_pos = self.pos.clone()
+        # becomes walked grid
+        ped_layer[self.pos[0], self.pos[1]] += AGENT_WALKED_PATH_PLUS
+        next_pos += self.action_to_move.get(action, torch.tensor((0, 0)))
+        self.pos = next_pos.clone()
+        # Update Agent Map
+        ped_layer[self.start[0], self.start[1]] = TYPE_MAP[self.type] + AGENT_START_PLUS
+        ped_layer[self.goal[0], self.goal[1]] = TYPE_MAP[self.type] + AGENT_GOAL_PLUS
+        ped_layer[self.pos[0], self.pos[1]] = TYPE_MAP[self.type]
+        return ped_layer
+
+    def get_global_action(self):
+        next_pos = self.global_traj[0]
+        self.global_traj.pop(0)
+        del_pos = tuple((next_pos - self.pos).tolist())
+        return self.move_to_action.get(del_pos, self.action_space[-1].item())
