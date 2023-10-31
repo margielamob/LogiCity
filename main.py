5cf099edb1c00c38c49d678b8851e3aeb27fa6c6
Bowen Li <bowenli2@andrew.cmu.edu>
2023-10-30 21:41:06 -0400

    save pkl updated

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def parse_arguments():
     parser.add_argument('--log_dir', type=str, default="./log")
     parser.add_argument('--exp', type=str, default="train_10k")
     parser.add_argument('--vis', type=bool, default=False, help='Visualize the city.')
-    parser.add_argument('--max-steps', type=int, default=100000, help='Maximum number of steps for the simulation.')
+    parser.add_argument('--max-steps', type=int, default=100, help='Maximum number of steps for the simulation.')
     parser.add_argument('--seed', type=int, default=18, help='random seed to use.')
     parser.add_argument('--debug', type=bool, default=False, help='In debug mode, the agents are in defined positions.')
 
