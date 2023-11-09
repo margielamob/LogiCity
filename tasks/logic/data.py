2937cfbf305abc66d7ae97787937cf0b020d7c89
Bowen Li <bowenli2@andrew.cmu.edu>
2023-11-09 11:46:19 -0500

    finished SATNet, it works good and converges faster than MLP

--- tasks/logic/data.py
+++ tasks/logic/data.py
@@ -1,3 +1,4 @@
+import torch
 import numpy as np
 from torch.utils.data import Dataset
 from prettytable import PrettyTable
@@ -66,3 +67,17 @@ class LogicDataset(Dataset):
 
     def __getitem__(self, idx):
         return self.dataX[idx], self.dataY[idx]
+
+class LogicDatasetSAT(LogicDataset):
+    def __init__(self, dataX, dataY, Xname, Yname, logger, adjust=False):
+        super().__init__(dataX, dataY, Xname, Yname, logger, adjust)
+        self.dataY = torch.tensor(np.where(dataY == 0.5, 0, 1)).float()
+        self.input_data = torch.cat((self.dataX, torch.zeros_like(self.dataY)), 1)
+        mask_list = [1]*self.dataX.shape[1] + [0]*self.dataY.shape[1]
+        self.input_mask = torch.IntTensor(mask_list).repeat(self.dataX.shape[0], 1)
+
+    def __len__(self):
+        return self.input_data.shape[0]
+
+    def __getitem__(self, idx):
+        return self.input_data[idx], self.input_mask[idx], self.dataY[idx]
