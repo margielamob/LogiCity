b5b93fa1d86275b30905b882c3e1d53353ab512d
Bowen Li <bowenli2@andrew.cmu.edu>
2023-12-28 16:00:54 -0500

    finished most part of the code, start debugging

--- planners/local/z3_local.py
+++ planners/local/z3_local.py
@@ -1,5 +1,6 @@
 import re
 import time
+import copy
 import torch
 import logging
 import importlib
@@ -12,6 +13,12 @@ from utils.sample import split_into_subsets
 from planners.local.basic import LocalPlanner
 
 logger = logging.getLogger(__name__)
+
+# used for grounding
+class PesudoAgent:
+    def __init__(self, type, layer_id):
+        self.type = type
+        self.layer_id = layer_id
 
 class Z3PlannerLocal(LocalPlanner):
     def __init__(self, yaml_path):        
@@ -37,11 +44,11 @@ class Z3PlannerLocal(LocalPlanner):
             if arity == 1:
                 # Unary predicate
                 entity_type = info["method"].split('(')[1].split(')')[0]
-                z3_func = "Function({method_name}, {entity_type}, BoolSort())"
+                z3_func = "Function('{}', entity_sorts['{}'], BoolSort())".format(method_name, entity_type)
             elif arity == 2:
                 # Binary predicate
                 types = info["method"].split('(')[1].split(')')[0].split(', ')
-                z3_func = "Function({method_name}, {types[0]}, {types[1]}, BoolSort())"
+                z3_func = "Function('{}', entity_sorts['{}'], entity_sorts['{}'], BoolSort())".format(method_name, types[0], types[1])
 
             # Store complete predicate information
             self.predicates[pred_name] = {
@@ -66,15 +73,14 @@ class Z3PlannerLocal(LocalPlanner):
 
             # Create Z3 variables based on the formula
             var_names = self._extract_variables(formula)
-            self.z3_vars = {var_name: Const(var_name, self.entity_types[var_name.replace('dummy', '')]) \
-                       for var_name in var_names}
+            self.z3_vars = var_names
 
             # Substitute predicate names in the formula with Z3 function instances
             for method_name, pred_info in self.predicates.items():
-                formula = formula.replace(method_name, f'predicates["{method_name}"]["instance"]')
+                formula = formula.replace(method_name, f'local_predicates["{method_name}"]["instance"]')
 
             # Now replace the variable names in the formula with their Z3 counterparts
-            for var_name, z3_var in self.z3_vars.items():
+            for var_name in var_names:
                 formula = formula.replace(var_name, f'z3_vars["{var_name}"]')
 
             # Evaluate the modified formula string to create the Z3 expression
@@ -101,47 +107,61 @@ class Z3PlannerLocal(LocalPlanner):
 
     def plan(self, world_matrix, intersect_matrix, agents):
         # 1. Break the global world matrix into local world matrix and split the agents and intersections
+        # Note that the local ones will have different size and agent id
         local_world_matrix = world_matrix.clone()
-        partial_agents, partial_world, partial_intersections = self.break_world_matrix(local_world_matrix, agents, intersect_matrix)
+        local_intersections = intersect_matrix.clone()
+        ego_agent, partial_agents, partial_world, partial_intersections = self.break_world_matrix(local_world_matrix, agents, local_intersections)
         # 2. multi-processing to solve each sub-problem
         with Pool(processes=NUM_PROCESS) as pool:
             results = pool.starmap(solve_sub_problem, 
-                                [(ego_name, self.rule_tem, self.entity_types, self.predicates, \
+                                [(ego_name, ego_agent[ego_name].action_mapping, ego_agent[ego_name].action_dist, \
+                                  self.rule_tem, self.entity_types, self.predicates, self.z3_vars, \
                                   partial_agents[ego_name], partial_world[ego_name], partial_intersections[ego_name]) \
                                     for ego_name in partial_agents.keys()])
 
         return results
     
-    def interpret_solution(self, model, agents, skip=False):
-        # Interpret the solution to the FOL problem
-        agents_actions = {}
-        for agent_entity in self.entities["Agent"]:
-            if "dummy" in agent_entity.decl().name():
-                continue
-            entity_name = agent_entity.decl().name()
-            agent = find_agent(agents, entity_name)
-            agent_name = "{}_{}".format(agent.type, agent.layer_id)
-            action_mapping = agent.action_mapping
-            action_dist = torch.zeros_like(agent.action_dist)
-
-            if not skip:
-                for key in self.predicates.keys():
-                    action = []
-                    for action_id, action_name in action_mapping.items():
-                        if key in action_name:
-                            action.append(action_id)
-                    if len(action)>0:
-                        for a in action:
-                            if is_true(model.evaluate(self.predicates[key]["instance"](agent_entity))):
-                                action_dist[a] = 1.0
-            # No action specified, use the default action, Normal
-            if action_dist.sum() == 0:
-                for action_id, action_name in action_mapping.items():
-                    if "Normal" in action_name:
-                        action_dist[action_id] = 1.0
-
-            agents_actions[agent_name] = action_dist
-        return agents_actions
+    def break_world_matrix(self, world_matrix, agents, intersect_matrix):
+        ego_agent = {}
+        partial_agents = {}
+        partial_world = {}
+        partial_intersection = {}
+        for agent in agents:
+            ego_name = "{}_{}".format(agent.type, agent.layer_id)
+            ego_agent[ego_name] = agent
+            ego_layer = world_matrix[agent.layer_id]
+            ego_position = (ego_layer == TYPE_MAP[agent.type]).nonzero()[0]
+                # Calculate the region of the city image that falls within the UAV's field of view
+            x_start = max(ego_position[0]-AGENT_FOV, 0)
+            y_start = max(ego_position[1]-AGENT_FOV, 0)
+            x_end = min(ego_position[0]+AGENT_FOV+1, world_matrix.shape[1])
+            y_end = min(ego_position[1]+AGENT_FOV+1, world_matrix.shape[2])
+            partial_world_all = world_matrix[:, x_start:x_end, y_start:y_end]
+            partial_intersections = intersect_matrix[:, x_start:x_end, y_start:y_end]
+            partial_world_nonzero_int = torch.logical_and(partial_world_all != 0, partial_world_all == partial_world_all.to(torch.int64))
+            # Apply torch.any across dimensions 1 and 2 sequentially
+            non_zero_layers = partial_world_nonzero_int.any(dim=1).any(dim=1)
+            non_zero_layer_indices = torch.where(non_zero_layers)[0]
+            partial_world_squeezed = partial_world_all[non_zero_layers]
+            partial_world[ego_name] = partial_world_squeezed
+            partial_intersection[ego_name] = partial_intersections
+            ego_agent_ = []
+            partial_agent = []
+            for layer_id in range(partial_world_squeezed.shape[0]):
+                layer = partial_world_squeezed[layer_id]
+                layer_nonzero_int = torch.logical_and(layer != 0, layer == layer.to(torch.int64))
+                if layer_nonzero_int.nonzero().shape[0] > 1:
+                    continue
+                non_zero_values = int(layer[layer_nonzero_int.nonzero()[0][0], layer_nonzero_int.nonzero()[0][1]])
+                agent_type = LABEL_MAP[non_zero_values]
+                # ego agent is the first
+                if non_zero_layer_indices[layer_id] == agent.layer_id:
+                    ego_agent_.append(PesudoAgent(agent_type, layer_id))
+                else:
+                    partial_agent.append(PesudoAgent(agent_type, layer_id))
+            partial_agents[ego_name] = ego_agent_ + partial_agent
+        return ego_agent, partial_agents, partial_world, partial_intersection
+            
 
     def format_rule_string(self, rule_str):
         indent_level = 0
@@ -170,22 +190,28 @@ class Z3PlannerLocal(LocalPlanner):
         return formatted_str
 
 def solve_sub_problem(ego_name, 
+                      ego_action_mapping,
+                      ego_action_dist,
                       rule_tem, 
                       entity_types, 
                       predicates, 
+                      var_names,
                       partial_agents, 
                       partial_world, 
                       partial_intersections):
     # 1. create solver
     local_solver = Solver()
-    # 2. create sorts
+    # 2. create sorts and variables
     entity_sorts = {}
     for entity_type in entity_types:
         entity_sorts[entity_type] = DeclareSort(entity_type)
+    z3_vars = {var_name: Const(var_name, entity_sorts[var_name.replace('dummy', '')]) \
+                       for var_name in var_names}
     # 3. partial world to entities
-    local_entities = world2entity(entity_sorts, partial_world, partial_intersections)
+    local_entities = world2entity(entity_sorts, partial_intersections, partial_agents)
     # 4. create, ground predicates and add to solver
-    for pred_name, pred_info in predicates.items():
+    local_predicates = copy.deepcopy(predicates)
+    for pred_name, pred_info in local_predicates.items():
         eval_pred = eval(pred_info["instance"])
         pred_info["instance"] = eval_pred
         arity = pred_info["arity"]
@@ -220,17 +246,46 @@ def solve_sub_problem(ego_name,
                         local_solver.add(Not(eval_pred(entity1, entity2)))
 
     # 5. create, ground rules and add to solver
-    for rule_name, rule_template in rule_tem.items():
+    local_rule_tem = copy.deepcopy(rule_tem)
+    for rule_name, rule_template in local_rule_tem.items():
         # the first entity is the ego agent
-        agent = local_entities[0]
+        agent = local_entities["Agent"][0]
         # Replace placeholder in the rule template with the actual agent entity
         instantiated_rule = eval(rule_template)
+        local_solver.add(instantiated_rule)
+
     # **Important: Closed world quantifier rule, to ensure z3 do not add new entity to satisfy the rule and "dummy" is not part of the world**
-    self.rules["ClosedWorld"] = []
-    for var_name, z3_var in self.z3_vars.items():
-        entity_list = self.entities[var_name.replace('dummy', '')]
+    for var_name, z3_var in z3_vars.items():
+        entity_list = local_entities[var_name.replace('dummy', '')]
         constraint = Or([z3_var == entity for entity in entity_list])
-        self.rules["ClosedWorld"].append(ForAll([z3_var], constraint))
+        local_solver.add(ForAll([z3_var], constraint))
+    
+    # 6. solve
+    if local_solver.check() == sat:
+        model = local_solver.model()
+        # Interpret the solution to the FOL problem
+        action_mapping = ego_action_mapping
+        action_dist = torch.zeros_like(ego_action_dist)
+
+        for key in local_predicates.keys():
+            action = []
+            for action_id, action_name in action_mapping.items():
+                if key in action_name:
+                    action.append(action_id)
+            if len(action)>0:
+                for a in action:
+                    if is_true(model.evaluate(local_predicates[key]["instance"](local_entities["Agent"][0]))):
+                        action_dist[a] = 1.0
+        # No action specified, use the default action, Normal
+        if action_dist.sum() == 0:
+            for action_id, action_name in action_mapping.items():
+                if "Normal" in action_name:
+                    action_dist[action_id] = 1.0
+
+        agents_actions = {ego_name: action_dist}
+        return agents_actions
+    else:
+        raise ValueError("No solution found for the local problem.")
 
 
 def world2entity(entity_sorts, partial_intersect, partial_agents):
