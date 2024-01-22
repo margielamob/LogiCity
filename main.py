243aee90d2d6152b86b7648ea3097b7e99869487
Bowen Li <bowenli2@perceptron.ri.cmu.edu>
2024-01-22 13:50:52 -0500

    don't change current logic, change the vis instead.

--- main.py
+++ main.py
@@ -31,16 +31,16 @@ def parse_arguments():
 
     # Add arguments for grid size, agent start and goal positions, etc.
     parser.add_argument('--map', type=str, default="config/maps/v1.1.yaml", help='YAML path to the map.')
-    parser.add_argument('--agents', type=str, default="config/agents/debug.yaml", help='YAML path to the agent definition.')
+    parser.add_argument('--agents', type=str, default="config/agents/v0.yaml", help='YAML path to the agent definition.')
     parser.add_argument('--rule_type', type=str, default="Z3_Local", help='We support ["LNN", "Z3_Global", "Z3_Local"].')
     parser.add_argument('--rules', type=str, default="config/rules/Z3/easy/easy_rule_local.yaml", help='YAML path to the rule definition.')
     # logger
     parser.add_argument('--log_dir', type=str, default="./log_rl")
-    parser.add_argument('--exp', type=str, default="occ_5")
+    parser.add_argument('--exp', type=str, default="easy_8_2k")
     parser.add_argument('--vis', type=bool, default=True, help='Visualize the city.')
-    parser.add_argument('--max-steps', type=int, default=120, help='Maximum number of steps for the simulation.')
+    parser.add_argument('--max-steps', type=int, default=1000, help='Maximum number of steps for the simulation.')
     parser.add_argument('--seed', type=int, default=1, help='random seed to use.')
-    parser.add_argument('--debug', type=bool, default=True, help='In debug mode, the agents are in defined positions.')
+    parser.add_argument('--debug', type=bool, default=False, help='In debug mode, the agents are in defined positions.')
     # RL
     parser.add_argument('--use_gym', type=bool, default=False, help='In gym mode, we can use RL alg. to control certain agents.')
     parser.add_argument('--rl_config', default='config/tasks/Nav/RL/config_0.001.yaml', help='Configure file for this RL exp.')
