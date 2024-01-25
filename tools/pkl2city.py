ed39e09521f6f8040da6dbda9671cc5d7b7eedb5
Bowen Li <bowenli2@andrew.cmu.edu>
2024-01-25 10:18:45 -0500

    local updates

--- tools/pkl2city.py
+++ tools/pkl2city.py
@@ -264,12 +264,16 @@ def paste_car_on_map(map_image, car_image, position, direction, type, position_l
                 head_position = (r, (t+b)//2)
                 new_position = (head_position[0] - rotated_car.width, head_position[1] - rotated_car.height//2)
         elif direction == 'none':
-            assert position_last is not None
-            new_position = tuple(position_last)
+            if position_last is not None:
+                new_position = tuple(position_last)
+            else:
+                new_position = (l, t)
     elif type == "Pedestrian":
         if direction == "none":
-            assert position_last is not None
-            new_position = tuple(position_last)
+            if position_last is not None:
+                new_position = tuple(position_last)
+            else:
+                new_position = (l, t)
         else:
             center_position = ((l+r)//2, (t+b)//2)
             new_position = (center_position[0] - rotated_car.width//2, center_position[1] - rotated_car.height//2)
@@ -432,13 +436,8 @@ def main(pkl_path, ego_id, output_folder):
 if __name__ == "__main__":
     # Parse command line arguments
     parser = argparse.ArgumentParser(description="Create an animated GIF from a sequence of images.")
-<<<<<<< HEAD
-    parser.add_argument("--pkl_file", default='log_rl/res18_model_rw2_1.pkl', help="Path to the folder containing image files.")
-    parser.add_argument("--ego_id", type=int, default=3, help="which agent is ego agent. Visualize the ego agent's start and goal. This is layer_id")
-=======
     parser.add_argument("--pkl_file", default='log/med_occ8_400.pkl', help="Path to the folder containing image files.")
     parser.add_argument("--ego_id", type=int, default=0, help="which agent is ego agent. Visualize the ego agent's start and goal. This is layer_id")
->>>>>>> ca2b6921637f2e839098467be86369a2e900a002
     parser.add_argument("--output_folder", default="vis_city", help="Output folder.")
     
     args = parser.parse_args()
