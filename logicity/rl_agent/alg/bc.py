737a96c7895bb74a69d984208efa03c086b250a1
Bowen Li <bowenli2@perceptron.ri.cmu.edu>
2024-02-05 20:43:42 -0500

    start coding for BC agent

--- logicity/rl_agent/alg/bc.py
+++ logicity/rl_agent/alg/bc.py
@@ -1,64 +1,32 @@
-import os
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader, TensorDataset
-import pickle
 
 class BCAgent:
-    def __init__(self, observation_space, action_space, model_architecture='mlp', learning_rate=1e-3):
-        self.observation_space = observation_space
-        self.action_space = action_space
-        self.model = self._build_model(model_architecture)
-        self.optimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
-        
-    def _build_model(self, architecture):
-        # Example for MLP architecture
-        if architecture == 'mlp':
-            model = nn.Sequential(
-                nn.Linear(self.observation_space.shape[0], 64),
-                nn.ReLU(),
-                nn.Linear(64, 64),
-                nn.ReLU(),
-                nn.Linear(64, self.action_space.n)  # Assuming discrete action space
-            )
-        else:
-            raise NotImplementedError(f"Architecture '{architecture}' is not implemented.")
-        return model
-    
-    def learn(self, demonstrations, batch_size=64, epochs=10):
-        # Convert demonstrations to PyTorch datasets
-        states, actions = zip(*demonstrations)
-        states = torch.tensor(np.array(states), dtype=torch.float32)
-        actions = torch.tensor(np.array(actions), dtype=torch.long)
-        dataset = TensorDataset(states, actions)
-        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-        
-        # Training loop
+    def __init__(self, policy_class, policy_kwargs, learning_rate=1e-3):
+        self.policy = policy_class(**policy_kwargs)
+        self.optimizer = optim.Adam(self.policy.parameters(), lr=learning_rate)
+        self.loss_fn = nn.MSELoss()  # Customize based on action space
+
+    def train(self, training_data, epochs=10, batch_size=64):
+        dataset = torch.utils.data.TensorDataset(torch.tensor(training_data['observations'], dtype=torch.float32),
+                                                 torch.tensor(training_data['actions'], dtype=torch.float32))
+        dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
         self.model.train()
         for epoch in range(epochs):
-            for batch_states, batch_actions in dataloader:
+            for observations, actions in dataloader:
+                predicted_actions = self.model(observations)
+                loss = self.loss_fn(predicted_actions, actions)
+                
                 self.optimizer.zero_grad()
-                action_logits = self.model(batch_states)
-                loss = nn.CrossEntropyLoss()(action_logits, batch_actions)
                 loss.backward()
                 self.optimizer.step()
-            print(f"Epoch {epoch+1}/{epochs}, Loss: {loss.item()}")
-    
-    def predict(self, observation, deterministic=True):
+            print(f'Epoch {epoch+1}, Loss: {loss.item()}')
+
+    def predict(self, observation):
         self.model.eval()
         with torch.no_grad():
             observation = torch.tensor(observation, dtype=torch.float32)
-            action_logits = self.model(observation)
-            if deterministic:
-                return torch.argmax(action_logits).item(), None
-            else:
-                # Implement stochastic action selection if needed
-                pass
-    
-    def save(self, path):
-        torch.save(self.model.state_dict(), path)
-    
-    def load(self, path):
-        self.model.load_state_dict(torch.load(path))
+            action = self.model(observation)
+        return action.numpy()
