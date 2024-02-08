05d86876ac607e16c84de7942a2ead343b2b89d6
Bowen Li <bowenli2@andrew.cmu.edu>
2024-02-08 09:37:39 -0500

    changes from local

---
+++ scripts/rl/test_expert.sh
@@ -0,0 +1,2 @@
+CONFIG=config/tasks/Nav/easy/experts/expert_test.yaml
+python3 main.py --config $CONFIG --exp expert_test --use_gym
--- scripts/rl/test_rl.sh
+++ scripts/rl/test_rl.sh
@@ -1,4 +1,4 @@
 # Description: Test RL agent
 # /bin/bash
 CONFIG=config/tasks/Nav/easy/RL/ppotest.yaml
-python3 main.py --config $CONFIG --exp car_test --use_gym
+python3 main.py --config $CONFIG --exp ppo3_3p6c2b --use_gym
