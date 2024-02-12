eee4529c7fccd070aea037cc23ae2d74ab07cd5d
Bowen Li <bowenli2@perceptron.ri.cmu.edu>
2024-02-12 13:22:59 -0500

    med rule debug

--- scripts/rl/train_rl.sh
+++ scripts/rl/train_rl.sh
@@ -1,1 +1,5 @@
-python3 main.py --config config/tasks/Nav/easy/RL/ppo.yaml --exp p3c6b2_ppo4 --use_gym
+CONFIG=config/tasks/Nav/easy/RL/ppo.yaml
+python3 main.py --config $CONFIG \
+    --exp train_ppo \
+    --log_dir log_rl \
+    --use_gym
