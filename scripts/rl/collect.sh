1eafa82c62f5a6ea92145ed6eefb42c3ef8ad426
Bowen Li <bowenli2@andrew.cmu.edu>
2024-02-19 16:42:26 -0500

    local changes

--- scripts/rl/collect.sh
+++ scripts/rl/collect.sh
@@ -1,5 +1,5 @@
 # Description: Collect Expert demonstration
 # /bin/bash
-CONFIG=config/tasks/Nav/easy/experts/expert_collect.yaml
-python3 main.py --config $CONFIG --exp expert_demon_10k \
+CONFIG=config/tasks/Nav/easy/experts/expert_collect_train.yaml
+python3 main.py --config $CONFIG --exp expert_demon_2k_0219 \
     --log_dir log_rl --collect_only
