603a9e683f08a87ba9f6bb284ded5f6da535fc11
Bowen Li <bowenli2@perceptron.ri.cmu.edu>
2024-02-21 17:00:23 -0500

    local changes

--- scripts/rl/episode.sh
+++ scripts/rl/episode.sh
@@ -4,8 +4,8 @@ conda activate logicity
 # /bin/bash
 # easy
 CONFIG_FILE="config/tasks/Nav/easy/experts/expert_episode_test.yaml"
-python3 tools/create_episode.py --config $CONFIG_FILE --exp "expert_episode_test_100" \
-    --max_episodes 100
-CONFIG_FILE="config/tasks/Nav/easy/experts/expert_episode_val.yaml"
-python3 tools/create_episode.py --config $CONFIG_FILE --exp "expert_episode_val_40" \
+python3 tools/create_episode.py --config $CONFIG_FILE --exp "expert_episode_test_40" \
     --max_episodes 40
+CONFIG_FILE="config/tasks/Nav/easy/experts/expert_episode_val.yaml"
+python3 tools/create_episode.py --config $CONFIG_FILE --exp "expert_episode_val_20" \
+    --max_episodes 20
