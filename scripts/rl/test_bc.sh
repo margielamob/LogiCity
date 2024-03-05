757efa445414370eba95dafdfcb2b6203950216d
Bowen Li <bowenli2@andrew.cmu.edu>
2024-03-05 09:39:32 -0500

    bc finished

--- scripts/rl/test_bc.sh
+++ scripts/rl/test_bc.sh
@@ -1,16 +1,10 @@
 source /opt/conda/etc/profile.d/conda.sh
 conda activate logicity
-for n in 1 2 5
+for n in 10 20 50 100
 do
-CONFIG=config/tasks/Nav/easy/RL/bctest_${n}k.yaml
+CONFIG=config/tasks/Nav/medium/algo/bc_${n}_test.yaml
 python3 main.py --config $CONFIG \
-    --exp test_bc_${n}k \
+    --exp test_bc_${n} \
     --log_dir log_rl \
     --use_gym
 done
-
-CONFIG=config/tasks/Nav/easy/RL/bctest_500.yaml
-python3 main.py --config $CONFIG \
-    --exp test_bc_500 \
-    --log_dir log_rl \
-    --use_gym
