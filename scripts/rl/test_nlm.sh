50a63bb3e84c341ebb0bf7c7503a85f612df217e
Bowen Li <bowenli2@andrew.cmu.edu>
2024-03-19 16:24:25 -0400

    testing NLM variants

--- scripts/rl/test_nlm.sh
+++ scripts/rl/test_nlm.sh
@@ -1,6 +1,12 @@
 # easy
-CONFIG=config/tasks/Nav/medium/algo/nlm_900.yaml
-python3 main.py --config $CONFIG \
-    --exp med_nlm_900 \
-    --log_dir log_rl \
-    --use_gym
+for n in 50 100
+    do
+    for c in 1 2 3 4 5
+    do
+        CONFIG=config/tasks/Nav/medium/algo/nlm_${n}_${c}.yaml
+        python3 main.py --config $CONFIG \
+            --exp med_nlm_${n}_${c} \
+            --log_dir log_rl \
+            --use_gym
+    done
+done
