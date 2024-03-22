5c399311a75220529a6b97e228f3b774159ce763
Bowen Li <bowenli2@andrew.cmu.edu>
2024-03-22 10:52:16 -0400

    nlm finished, looks good

--- scripts/rl/test_nlm_arg.sh
+++ scripts/rl/test_nlm_arg.sh
@@ -1,15 +1,15 @@
 # med
 CONFIG=config/tasks/Nav/medium/algo/nlm.yaml
-for c in 1 2 3 4 5
+for c in 4 5
 do
-    python3 main.py --config $CONFIG \
-        --exp med_nlm_50_${c} \
-        --checkpoint_path checkpoints/final_models/medium/nlm_50/checkpoint_${c}.pth \
-        --log_dir log_rl \
-        --use_gym
     python3 main.py --config $CONFIG \
         --exp med_nlm_100_${c} \
         --checkpoint_path checkpoints/final_models/medium/nlm_100/checkpoint_${c}.pth \
         --log_dir log_rl \
         --use_gym
 done
+python3 main.py --config $CONFIG \
+    --exp med_nlm_50_5 \
+    --checkpoint_path checkpoints/final_models/medium/nlm_50/checkpoint_5.pth \
+    --log_dir log_rl \
+    --use_gym
