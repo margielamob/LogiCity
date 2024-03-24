00c17b3331c36f4fef2e9b65202f1844c58cf89d
Bowen Li <bowenli2@andrew.cmu.edu>
2024-03-24 12:59:57 -0400

    merged from huangzhong, zhangfei updates for DQN

--- scripts/rl/train_dqn.sh
+++ scripts/rl/train_dqn.sh
@@ -1,5 +1,5 @@
-CONFIG=config/tasks/Nav/hardium/algo/dqn.yaml
+CONFIG=config/tasks/Nav/easy_med/algo/dqn.yaml
 python3 main.py --config $CONFIG \
-    --exp medium_dqn \
+    --exp easy_med_dqn \
     --log_dir log_rl \
     --use_gym
