a7167109927866ce4174e30c1b8d658e9958943b
Bowen Li <bowenli2@andrew.cmu.edu>
2024-03-23 21:45:46 -0400

    local updates on huangzhong

--- scripts/rl/test_nlm_arg.sh
+++ scripts/rl/test_nlm_arg.sh
@@ -1,15 +1,15 @@
 # med
-CONFIG=config/tasks/Nav/medium/algo/nlm.yaml
+CONFIG=config/tasks/Nav/easy/algo/nlm.yaml
 for c in 1 2 3 4 5
 do
     python3 main.py --config $CONFIG \
-        --exp med_nlm_50_${c} \
-        --checkpoint_path checkpoints/final_models/medium/nlm_50/checkpoint_${c}.pth \
-        --log_dir log_rl \
-        --use_gym
-    python3 main.py --config $CONFIG \
-        --exp med_nlm_100_${c} \
-        --checkpoint_path checkpoints/final_models/medium/nlm_100/checkpoint_${c}.pth \
+        --exp easy_nlm_50_${c} \
+        --checkpoint_path checkpoints/final_models/easy/nlm_50/checkpoint_${c}.pth \
         --log_dir log_rl \
         --use_gym
 done
+python3 main.py --config $CONFIG \
+    --exp easy_nlm_100_5 \
+    --checkpoint_path checkpoints/final_models/easy/nlm_100/checkpoint_5.pth \
+    --log_dir log_rl \
+    --use_gym
