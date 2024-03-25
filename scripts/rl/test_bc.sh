9cb2c7bd1c3b9b64c83cb0ea92b4d94b6b8d71eb
Bowen Li <bowenli2@andrew.cmu.edu>
2024-03-25 16:06:46 -0400

    bc for easymed

--- scripts/rl/test_bc.sh
+++ scripts/rl/test_bc.sh
@@ -1,15 +1,15 @@
 # source /opt/conda/etc/profile.d/conda.sh
 # conda activate logicity
-# for n in 200 500 800 1000
-# do
-# CONFIG=config/tasks/Nav/hardium/algo/bc_${n}_test.yaml
-# python3 main.py --config $CONFIG \
-#     --exp test_bc_${n} \
-#     --log_dir log_rl \
-#     --use_gym
-# done
-
-python3 main.py --config config/tasks/Nav/hardium/experts/expert_test.yaml \
-    --exp test_expert2 \
+for n in 50 100 200 500 800 1000
+do
+CONFIG=config/tasks/Nav/easy_med/algo/bc${n}_test.yaml
+python3 main.py --config $CONFIG \
+    --exp test_bc_${n} \
     --log_dir log_rl \
     --use_gym
+done
+
+# python3 main.py --config config/tasks/Nav/hardium/experts/expert_test.yaml \
+#     --exp test_expert2 \
+#     --log_dir log_rl \
+#     --use_gym
