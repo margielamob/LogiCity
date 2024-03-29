767c1e5f6d527db1b18c1f301ff94adbf9f0665b
Bowen Li <bowenli2@andrew.cmu.edu>
2024-03-28 22:08:05 -0400

    zhangfe

--- scripts/rl/test_nlm_arg.sh
+++ scripts/rl/test_nlm_arg.sh
@@ -1,17 +1,17 @@
 # med
 CONFIG=config/tasks/Nav/easy_med/algo/nlm.yaml
-for c in 4
+for c in 5
 do
     python3 main.py --config $CONFIG \
-        --exp easymed_nlm_100_val_${c} \
+        --exp easymed_nlm_100_test_${c} \
         --checkpoint_path checkpoints/nlm_easy_med_100/checkpoints/checkpoint_${c}.pth \
         --log_dir log_rl \
         --use_gym
 done
-for c in 5
+for c in 4
 do
     python3 main.py --config $CONFIG \
-        --exp easymed_nlm_50_val_${c} \
+        --exp easymed_nlm_50_test_${c} \
         --checkpoint_path checkpoints/nlm_easy_med_50/checkpoints/checkpoint_${c}.pth \
         --log_dir log_rl \
         --use_gym
