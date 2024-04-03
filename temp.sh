b19038d663347a26f877a70451cef2107c6fec76
Bowen Li <bowenli2@andrew.cmu.edu>
2024-04-03 10:37:31 -0400

    latest on huangzhong

--- temp.sh
+++
@@ -1,5 +0,0 @@
-python3 main.py --config config/tasks/Nav/easy/experts/expert_collect_train.yaml \
-    --exp collect_east --collect_only
-python3 main.py --config config/tasks/Nav/medium/experts/expert_collect_train.yaml \
-    --exp collect_medium --collect_only
-# python3 tools/create_episode.py
