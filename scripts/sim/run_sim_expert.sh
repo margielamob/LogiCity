6199d5b11ea4349d2652bdac1f27aa6fc42f700a
Bowen Li <bowenli2@andrew.cmu.edu>
2024-04-03 11:58:20 -0400

    latest

--- scripts/sim/run_sim_expert.sh
+++ scripts/sim/run_sim_expert.sh
@@ -1,11 +1,11 @@
 # source /opt/conda/etc/profile.d/conda.sh
 # conda activate logicity
-EXPNAME="med_1k"
+EXPNAME="expert_1k"
 MAXSETP=1000
 
 for s in 0 1 2 3 4
 do
-python3 main.py --config "config/tasks/sim/med.yaml" \
+python3 main.py --config "config/tasks/sim/expert.yaml" \
         --exp ${EXPNAME}_${s} --max-steps $MAXSETP --seed $s \
         --log_dir log_sim
 done
