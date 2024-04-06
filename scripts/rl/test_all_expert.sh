1862072e0dc63849b4899e964f8ec5025d818da9
Bowen Li <bowenli2@andrew.cmu.edu>
2024-04-06 13:04:53 -0400

    testing hri mdei

--- scripts/rl/test_all_expert.sh
+++ scripts/rl/test_all_expert.sh
@@ -1,5 +1,11 @@
 MODE=expert
 
+for iter in 2000 3000
+do
+python3 main.py --use_gym --config config/tasks/Nav/easy/algo/hritest.yaml --exp easy_hri_test_${iter} \
+    --checkpoint_path checkpoints/final_models/easy/hri_${iter}
+done
+
 # python3 main.py --config config/tasks/Nav/${MODE}/algo/random_test.yaml \
 #     --exp random_${MODE}_test \
 #     --log_dir log_rl \
@@ -39,26 +45,26 @@ MODE=expert
 #     --log_dir log_rl \
 #     --use_gym
 
-python3 main.py --config config/tasks/Nav/${MODE}/algo/a2c_test.yaml \
-    --exp a2c_${MODE} \
-    --checkpoint_path checkpoints/final_models/${MODE}/a2c.zip \
-    --log_dir log_rl \
-    --use_gym
+# python3 main.py --config config/tasks/Nav/${MODE}/algo/a2c_test.yaml \
+#     --exp a2c_${MODE} \
+#     --checkpoint_path checkpoints/final_models/${MODE}/a2c.zip \
+#     --log_dir log_rl \
+#     --use_gym
 
-python3 main.py --config config/tasks/Nav/${MODE}/algo/ppo_test.yaml \
-    --exp ppo_${MODE} \
-    --checkpoint_path checkpoints/final_models/${MODE}/ppo.zip \
-    --log_dir log_rl \
-    --use_gym
+# python3 main.py --config config/tasks/Nav/${MODE}/algo/ppo_test.yaml \
+#     --exp ppo_${MODE} \
+#     --checkpoint_path checkpoints/final_models/${MODE}/ppo.zip \
+#     --log_dir log_rl \
+#     --use_gym
 
-for c in 1 2 3 4 5
-do
-python3 main.py --config config/tasks/Nav/${MODE}/algo/nlmval.yaml \
-    --exp nlmbc_${c}_${MODE}_val \
-    --checkpoint_path checkpoints/logicity_${MODE}100/checkpoints/checkpoint_${c}.pth \
-    --log_dir log_rl \
-    --use_gym
-done
+# for c in 1 2 3 4 5
+# do
+# python3 main.py --config config/tasks/Nav/${MODE}/algo/nlmval.yaml \
+#     --exp nlmbc_${c}_${MODE}_val \
+#     --checkpoint_path checkpoints/logicity_${MODE}100/checkpoints/checkpoint_${c}.pth \
+#     --log_dir log_rl \
+#     --use_gym
+# done
 
 # python3 main.py --config config/tasks/Nav/${MODE}/algo/hri_test.yaml \
 #     --exp hri_${MODE} \
