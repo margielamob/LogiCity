aa91f7f6162f972ea6f3f44809783fabd1a2e5bc
Bowen Li <bowenli2@andrew.cmu.edu>
2024-05-05 16:12:41 -0400

    finished testing maxsynth agent, testing medium and hard

--- scripts/rl/episode_medium.sh
+++ scripts/rl/episode_medium.sh
@@ -3,9 +3,9 @@ source /opt/conda/etc/profile.d/conda.sh
 conda activate logicity
 # /bin/bash
 # easy
-CONFIG_FILE="config/tasks/Nav/transfer/medium/expert_episode_test.yaml"
-python3 tools/create_episode.py --config $CONFIG_FILE --exp "medium_test_transfer" \
-    --max_episodes 100
+# CONFIG_FILE="config/tasks/Nav/transfer/medium/expert_episode_test.yaml"
+# python3 tools/create_episode.py --config $CONFIG_FILE --exp "medium_test_transfer" \
+#     --max_episodes 100
 CONFIG_FILE="config/tasks/Nav/transfer/medium/expert_episode_val.yaml"
 python3 tools/create_episode.py --config $CONFIG_FILE --exp "medium_val_transfer" \
     --max_episodes 40
