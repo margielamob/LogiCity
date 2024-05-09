c4bd5e9ba3b2c8966db509c60558db8893fc4dca
Bowen Li <bowenli2@andrew.cmu.edu>
2024-05-09 10:26:45 -0400

    zhangfei local

--- scripts/rl/test_all.sh
+++ scripts/rl/test_all.sh
@@ -68,11 +68,11 @@ MODE=easy
 #     --use_gym
 # done
 
-# for iter in 120000 140000
-# do
-# python3 main.py --use_gym --config config/tasks/Nav/easy/algo/a2ctest.yaml --exp easy_a2c_test_${iter} \
-#     --checkpoint_path checkpoints/easy_a2c_${iter}_steps.zip
-# done
+for iter in {5000..60000..5000}
+do
+python3 main.py --use_gym --config config/tasks/Nav/transfer/medium/algo/dqn_test.yaml --exp transfer_easy2medium_dqn_test_${iter} \
+    --checkpoint_path checkpoints/transfer_dqn_medium_transfer_${iter}_steps.zip
+done
 
 # python3 main_es.py --config config/tasks/Nav/easy/algo/dqn_estest.yaml --exp easy_dqnes_test
 
@@ -82,9 +82,9 @@ MODE=easy
 
 # python3 main_es.py --config config/tasks/Nav/hard/algo/mbrl_estest.yaml --exp hard_mbrles_test
 
-python3 main.py --config config/tasks/Nav/transfer/easy/algo/dqn_test_train.yaml --exp transfer_easy_dqn_test_train --use_gym
+# python3 main.py --config config/tasks/Nav/transfer/easy/algo/dqn_test_train.yaml --exp transfer_easy_dqn_test_train --use_gym
 
-python3 transfer.py --config config/tasks/Nav/transfer/medium/algo/dqn_transfer.yaml --exp transfer_easy2medium_dqn_train1 --use_gym
+# python3 transfer.py --config config/tasks/Nav/transfer/medium/algo/dqn_transfer.yaml --exp transfer_easy2medium_dqn_train1
 # for iter in 100000
 # do
 # python3 main.py --config config/tasks/Nav/transfer/easy/algo/dqn_test.yaml --exp transfer_easy_dqn_test_${iter} \
